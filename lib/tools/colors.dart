f0b2df81ae078c61fe6dff62002ff6755396b49c
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-07-26 11:27:11 +0430

    laptop_store_ui

---
+++ lib/tools/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const color1=Color(0xff02529B);
+const color2=Color(0xff060756);
+const color3=Color(0xffDCDFE4);
+const color4=Color(0xffECF0F1);
+const color5=Color(0xff13111D);
