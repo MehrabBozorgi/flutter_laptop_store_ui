3394212a198da3dde0483a73fd99f584b0a7ca53
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-07-26 11:26:57 +0430

    laptop_store_ui

---
+++ lib/tools/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const textBlackStyle1=TextStyle(color: color5,fontSize:24,fontWeight: FontWeight.bold, );
+const textBlackStyle2=TextStyle(color: color5,fontSize:20,fontWeight: FontWeight.bold, );
+const textBlackStyle3=TextStyle(color: color5,fontSize:18,fontWeight: FontWeight.bold, );
+const textBlackStyle4=TextStyle(color: color5,fontSize:16,fontWeight: FontWeight.bold, );
+const textBlackStyle5=TextStyle(color: color5,fontSize:16,fontWeight: FontWeight.normal, );
+const textBlackStyle6=TextStyle(color: color5,fontSize:14,fontWeight: FontWeight.bold, );
+//
+const textGreyStyle1=TextStyle(color: Color(0xFF616161),fontSize:14,fontWeight: FontWeight.bold, );
+const textGreyStyle2=TextStyle(color: Color(0xFF616161),fontSize:16,fontWeight: FontWeight.bold, );
+//
+const textWhiteStyle1=TextStyle(color: Colors.white,fontSize:16,fontWeight: FontWeight.bold, );
+const textWhiteStyle2=TextStyle(color: Colors.white,fontSize:14,fontWeight: FontWeight.bold, );
